8a81c87be00f0017ef418c847803ebbc1d018c6d
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-07-18 18:08:58 +0700

    Setup

---
+++ 331-intro-to-vite/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import EventCard from './components/EventCard.vue'
+import StudentCard from './components/StudentCard.vue'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Event</RouterLink>
+          <RouterLink to="/students">Student</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style>
+#layout {
+  font-family: Avinir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+h2 {
+  font-size: 20px;
+}
+</style>
